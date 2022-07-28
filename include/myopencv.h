3b528431cb22acfec326f7f60baa027832eafff9
Nao-Chu <1206278457@qq.com>
2022-07-28 19:53:42 +0800

    优化代码

--- include/myopencv.h
+++ include/myopencv.h
@@ -1,5 +1,38 @@
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
+
+
+template<class T>
+class CompareMin
+{
+public:
+	CompareMin (T v)
+	{
+		initv = v;
+	} 
+	T operator() (T a, T b) {
+		return (a < b) ? a : b;
+	}
+	T initv;
+};
+
+template<class T>
+class CompareMax
+{
+public:
+	CompareMax (T v)
+	{
+		initv = v;
+	} 
+	T operator() (T a, T b) {
+		return (a > b) ? a : b;
+	}
+	T initv;
+};
+
+template<class Compare>
+void convolution(Mat& src, Mat& dst, Mat& ele, Compare comp);
+
 void merode(Mat& src, Mat& dst, Mat& ele);
 void mdilate(Mat& src, Mat& dst, Mat& ele);
--- myopencv.cpp
+++ myopencv.cpp
@@ -3,15 +3,16 @@
 #include <iostream>
 using namespace std;
 
-void merode(Mat& src, Mat& dst, Mat& ele)
+
+template<class Compare>
+void convolution(Mat& src, Mat& dst, Mat& ele, Compare comp)
 {
 	dst = Mat::zeros(src.size(), src.type());
 	int cx = ele.rows >> 1;	
 	int cy = ele.cols >> 1;	
 	for (int i = 0; i < src.rows; ++i) {
 		for (int j = 0; j < src.cols; ++j) {
-			bool is_full = true;
-			uchar min_v[3] = {255, 255, 255};
+			uchar convolution_v[3] = {comp.initv, comp.initv, comp.initv};
 			for (int k = -cx; k <= cx; ++k) {
 				for (int l = -cy; l <= cy; ++l) {
 
@@ -23,88 +24,37 @@ void merode(Mat& src, Mat& dst, Mat& ele)
 						continue;
 					}
 
-					int v = ele.at<uchar>(k+cx, l+cy); 
 					if (dst.channels() == 1) {
-						is_full = src.at<uchar>(dx,dy) & v;
+						src.at<uchar>(dx,dy) = comp(convolution_v[0], src.at<uchar>(dx,dy));
 					} else {
-						min_v[0] = std::min(min_v[0], src.at<Vec3b>(dx,dy)[0]);
-						min_v[1] = std::min(min_v[1], src.at<Vec3b>(dx,dy)[1]);
-						min_v[2] = std::min(min_v[2], src.at<Vec3b>(dx,dy)[2]);
+						convolution_v[0] = comp(convolution_v[0], src.at<Vec3b>(dx,dy)[0]);
+						convolution_v[1] = comp(convolution_v[1], src.at<Vec3b>(dx,dy)[1]);
+						convolution_v[2] = comp(convolution_v[2], src.at<Vec3b>(dx,dy)[2]);
 					}
 
-					if (!is_full) {
-						break;
-					}
-				}
-				if (!is_full) {
-					break;
 				}
 				
 			}
 			
-			if (is_full) {
-				if (dst.channels() == 1) {
-					dst.at<uchar>(i,j) = 255;
-				} else {
-					dst.at<Vec3b>(i,j)[0] = min_v[0];
-					dst.at<Vec3b>(i,j)[1] = min_v[1];
-					dst.at<Vec3b>(i,j)[2] = min_v[2];
-				}
+			if (dst.channels() == 1) {
+				dst.at<uchar>(i,j) = convolution_v[0];
+			} else {
+				dst.at<Vec3b>(i,j)[0] = convolution_v[0];
+				dst.at<Vec3b>(i,j)[1] = convolution_v[1];
+				dst.at<Vec3b>(i,j)[2] = convolution_v[2];
 			}
 
 		}
 	}
 }
-
-
+void merode(Mat& src, Mat& dst, Mat& ele)
+{
+	CompareMin<uchar> func(255);
+	convolution(src, dst, ele, func);
+}
 
 void mdilate(Mat& src, Mat& dst, Mat& ele)
 {
-	dst = Mat::zeros(src.size(), src.type());
-	int cx = ele.rows >> 1;	
-	int cy = ele.cols >> 1;	
-	for (int i = 0; i < src.rows; ++i) {
-		for (int j = 0; j < src.cols; ++j) {
-			bool is_full = false;
-			uchar max_v[3] = {0, 0, 0};
-			for (int k = -cx; k <= cx; ++k) {
-				for (int l = -cy; l <= cy; ++l) {
-
-					int dx = i + k;	
-					int dy = j + l;	
-					if (dx < 0 || dx >= src.rows|| 
-						dy < 0 || dy >= src.cols) {
-						
-						continue;
-					}
-
-					int v = ele.at<uchar>(k+cx, l+cy); 
-					if (dst.channels() == 1) {
-						is_full = src.at<uchar>(dx,dy) & v;
-					} else {
-						max_v[0] = std::max(max_v[0], src.at<Vec3b>(dx,dy)[0]);
-						max_v[1] = std::max(max_v[1], src.at<Vec3b>(dx,dy)[1]);
-						max_v[2] = std::max(max_v[2], src.at<Vec3b>(dx,dy)[2]);
-					}
-
-					if (is_full) {
-						break;
-					}
-				}
-				if (is_full) {
-					break;
-				}
-				
-			}
-			
-			if (is_full && dst.channels() == 1) {
-				dst.at<uchar>(i,j) = 255;
-			} else if (dst.channels() == 3) {
-				dst.at<Vec3b>(i,j)[0] = max_v[0];
-				dst.at<Vec3b>(i,j)[1] = max_v[1];
-				dst.at<Vec3b>(i,j)[2] = max_v[2];
-			}
-		}
-	} 
-	//dst = src;
+	CompareMax<uchar> func(0);
+	convolution(src, dst, ele, func);
 }
