a071f1c8d691930e81fbe6bc1e00d58d9fb53c08
Nao-Chu <1206278457@qq.com>
2022-07-28 20:49:26 +0800

    增加半成品代码

--- include/myopencv.h
+++ include/myopencv.h
@@ -1,34 +1,42 @@
 #include <opencv2/opencv.hpp>
 
+#include <vector>
+#include<numeric>
 using namespace cv;
-
+using namespace std;
 
 template<class T>
 class CompareMin
 {
 public:
-	CompareMin (T v)
+	T operator() (vector<T> data, vector<T> ele) 
 	{
-		initv = v;
-	} 
-	T operator() (T a, T b) {
-		return (a < b) ? a : b;
+		return *std::min_element(data.begin(), data.end());
 	}
-	T initv;
 };
 
 template<class T>
 class CompareMax
 {
 public:
-	CompareMax (T v)
+	T operator() (vector<T> data, vector<T> ele) 
 	{
-		initv = v;
-	} 
-	T operator() (T a, T b) {
-		return (a > b) ? a : b;
+		return *std::max_element(data.begin(), data.end());
 	}
-	T initv;
+};
+
+template<class T>
+class CompareMean
+{
+public:
+	T operator() (vector<T> data, vector<T> ele) 
+	{
+		T acc;
+		for (int i = 0; i < data.size(); ++i) {
+			acc += data[i]*ele[i];
+		}
+		return acc / data.size();
+	}
 };
 
 template<class Compare>
@@ -36,3 +44,4 @@ void convolution(Mat& src, Mat& dst, Mat& ele, Compare comp);
 
 void merode(Mat& src, Mat& dst, Mat& ele);
 void mdilate(Mat& src, Mat& dst, Mat& ele);
+void mblur(Mat& src, Mat& dst, Size size);
--- myopencv.cpp
+++ myopencv.cpp
@@ -5,14 +5,19 @@ using namespace std;
 
 
 template<class Compare>
-void convolution(Mat& src, Mat& dst, Mat& ele, Compare comp)
+void convolution(Mat& src, Mat& dst, vector<vector<int>> ele, Compare comp)
 {
 	dst = Mat::zeros(src.size(), src.type());
-	int cx = ele.rows >> 1;	
-	int cy = ele.cols >> 1;	
+
+	if (ele.size() == 0) {
+		return;
+	}
+	int cx = ele.size() >> 1;	
+	int cy = ele[0].size() >> 1;	
 	for (int i = 0; i < src.rows; ++i) {
 		for (int j = 0; j < src.cols; ++j) {
-			uchar convolution_v[3] = {comp.initv, comp.initv, comp.initv};
+			vector<vector<uchar> > convolution_v(3);
+			vector<int> ele_v(3);
 			for (int k = -cx; k <= cx; ++k) {
 				for (int l = -cy; l <= cy; ++l) {
 
@@ -25,11 +30,13 @@ void convolution(Mat& src, Mat& dst, Mat& ele, Compare comp)
 					}
 
 					if (dst.channels() == 1) {
-						src.at<uchar>(dx,dy) = comp(convolution_v[0], src.at<uchar>(dx,dy));
+						convolution_v[0].push_back(src.at<uchar>(dx,dy));
+						ele_v.push_back(ele[k+cx, l+cy]);
 					} else {
-						convolution_v[0] = comp(convolution_v[0], src.at<Vec3b>(dx,dy)[0]);
-						convolution_v[1] = comp(convolution_v[1], src.at<Vec3b>(dx,dy)[1]);
-						convolution_v[2] = comp(convolution_v[2], src.at<Vec3b>(dx,dy)[2]);
+						convolution_v[0].push_back(src.at<Vec3b>(dx,dy)[0]);
+						convolution_v[1].push_back(src.at<Vec3b>(dx,dy)[1]);
+						convolution_v[2].push_back(src.at<Vec3b>(dx,dy)[2]);
+						ele_v.push_back(ele[k+cx, l+cy]);
 					}
 
 				}
@@ -37,11 +44,11 @@ void convolution(Mat& src, Mat& dst, Mat& ele, Compare comp)
 			}
 			
 			if (dst.channels() == 1) {
-				dst.at<uchar>(i,j) = convolution_v[0];
+				dst.at<uchar>(i,j) = comp(convolution_v[0], ele_v);
 			} else {
-				dst.at<Vec3b>(i,j)[0] = convolution_v[0];
-				dst.at<Vec3b>(i,j)[1] = convolution_v[1];
-				dst.at<Vec3b>(i,j)[2] = convolution_v[2];
+				dst.at<Vec3b>(i,j)[0] = comp(convolution_v[0], ele_v);
+				dst.at<Vec3b>(i,j)[1] = comp(convolution_v[1], ele_v);
+				dst.at<Vec3b>(i,j)[2] = comp(convolution_v[2], ele_v);
 			}
 
 		}
@@ -49,12 +56,25 @@ void convolution(Mat& src, Mat& dst, Mat& ele, Compare comp)
 }
 void merode(Mat& src, Mat& dst, Mat& ele)
 {
-	CompareMin<uchar> func(255);
+	CompareMin<uchar> func;
 	convolution(src, dst, ele, func);
 }
 
 void mdilate(Mat& src, Mat& dst, Mat& ele)
 {
-	CompareMax<uchar> func(0);
+	CompareMax<uchar> func;
+	convolution(src, dst, ele, func);
+}
+
+void mblur(Mat& src, Mat& dst, Size size)
+{
+	vector<vector<float>> ele;
+	int ss = size.height*size.width;
+	for (int i = 0; i < size.height; ++i) {
+		for (int i = 0; i < size.width; ++i) {
+			ele[i][j] = 1/ss;
+		}
+	}
+	CompareMean<uchar> func;
 	convolution(src, dst, ele, func);
 }
